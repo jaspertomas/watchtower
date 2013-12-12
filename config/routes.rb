49b1d55b757f1492933a7d1add99e31492512575
Jasper Tomas <jaspertomas@gmail.com>
2013-12-12 22:18:31 +0800

    started building sessions module (login / logout)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 FirstApp::Application.routes.draw do
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
