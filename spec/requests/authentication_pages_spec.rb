becfbd396746daf29f16b0fda1a4c328d49429fa
Jasper Tomas <jaspertomas@gmail.com>
2013-12-12 23:01:41 +0800

    login logout ok

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,11 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
     
     describe "with valid information" do
@@ -27,6 +32,10 @@ describe "Authentication" do
         fill_in "Email",    with: user.email.upcase
         fill_in "Password", with: user.password
         click_button "Sign in"
+      end
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
       end
 
       it { should have_title(user.name) }
